da2fcac30735284c4afd33c17b8afeeaf5ef9e5c
zvolankm <zvolankovamartina@gmail.com>
2022-10-24 09:27:05 +0200

    Vizitkovník 2 – formuláře V2

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -5,11 +5,8 @@ import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
@@ -28,11 +25,32 @@ public class VizitkaController {
     result.addObject("seznam", service.getAll());
     return result;
   }
-
   @GetMapping("/detail/{id}")
   public ModelAndView detail(@PathVariable int id) {
     ModelAndView result = new ModelAndView("detail");
     result.addObject("vizitka", service.getById(id));
     return result;
   }
+
+  @GetMapping("/nova")
+  public ModelAndView showAppendForm() {
+    ModelAndView result = new ModelAndView("pridani");
+    System.out.println("Vytvarim formular pro pridani vizitky.");
+    return result;
+  }
+
+  @PostMapping(path = "/")
+  public String append(Vizitka vizitka){
+    System.out.println("Pridavam novou vizitku do kolekce.");
+    service.append(vizitka);
+    return "redirect:/ ";
+  }
+
+  @PostMapping(path = "/delete", params = "id")
+  public String delete(int id){
+    System.out.println("Odmazavam vizitku z kolekce");
+    service.deleteById(id);
+    return "redirect:/ ";
+  }
+
 }
--- src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -18,21 +18,24 @@ public class Vizitka {
   }
 
   public Vizitka(String jmeno, String firma, String ulice, String obecPsc, String email, String telefon, String web) {
-    this.jmeno = Objects.requireNonNull(jmeno);
-    this.firma = Objects.requireNonNull(firma);
-    this.ulice = Objects.requireNonNull(ulice);
-    this.obecPsc = Objects.requireNonNull(obecPsc);
+    this.jmeno = jmeno;
+    this.firma = firma;
+    this.ulice = ulice;
+    this.obecPsc = obecPsc;
     this.email = email;
     this.telefon = telefon;
     this.web = web;
   }
 
+
+
   public String getJmeno() {
     return jmeno;
   }
 
+
   public void setJmeno(String jmeno) {
-    this.jmeno = Objects.requireNonNull(jmeno);
+    this.jmeno = jmeno;
   }
 
   public String getFirma() {
@@ -40,7 +43,7 @@ public class Vizitka {
   }
 
   public void setFirma(String firma) {
-    this.firma = Objects.requireNonNull(firma);
+    this.firma = firma;
   }
 
   public String getUlice() {
@@ -48,42 +51,36 @@ public class Vizitka {
   }
 
   public void setUlice(String ulice) {
-    this.ulice = Objects.requireNonNull(ulice);
+    this.ulice = ulice;
   }
 
   public String getObecPsc() {
     return obecPsc;
-  }
+        }
 
-  public void setObecPsc(String obecPsc) {
-    this.obecPsc = Objects.requireNonNull(obecPsc);
-  }
+      public void setObecPSc(String obecPsc) {
+        this.obecPsc = obecPsc;
+      }
 
-  public String getEmail() {
-    return email;
+      public String getEmail() {
+        return email;
+      }
+      public void setEmail(String email) {
+        this.email = email;
+      }
+      public String getTelefon() {
+        return telefon;
+      }
+      public void setTelefon(String telefon) {
+        this.telefon = telefon;
+      }
+      public String getWeb() {
+        return web;
+      }
+      public void setWeb(String web) {
+        this.web = web;
+      }
+      public String getCelaAdresa() {
+      return String.format("%s, %s", ulice, obecPsc);
   }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getTelefon() {
-    return telefon;
-  }
-
-  public void setTelefon(String telefon) {
-    this.telefon = telefon;
-  }
-
-  public String getWeb() {
-    return web;
-  }
-
-  public void setWeb(String web) {
-    this.web = web;
-  }
-
-  public String getCelaAdresa() {
-    return ulice + ", " + obecPsc;
-  }
-}
+    }
